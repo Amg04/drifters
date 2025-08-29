ac8906bc0ae8fb8742dc4e16213274eef936fa8d
Amg04 <am4883721@gmail.com>
2025-08-29 20:52:44 +0300

    Add project files.

---
+++ PL/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using BLLProject.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AccountController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return NotFound();
+        }
+    }
+}
